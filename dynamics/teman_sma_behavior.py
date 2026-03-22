22a3e8c0417f8de69a5a3c912389654e74cefcc9
asyahril-sha <asyahril.aiart@gmail.com>
2026-03-22 18:30:14 +0800

    Update teman_sma_behavior.py

--- dynamics/teman_sma_behavior.py
+++ dynamics/teman_sma_behavior.py
@@ -20,7 +20,7 @@ import time
 from typing import Dict, List, Optional, Any
 
 from .role_behavior import RoleBehavior
-from config.role_behavior_config import TEMAN_SMA_CONFIG
+from configs.role_behavior_config import TEMAN_SMA_CONFIG
 
 
 class TemanSmaBehavior(RoleBehavior):
